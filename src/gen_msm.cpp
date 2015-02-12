62c417f465d2aa25829086e7d790442d5494d46d
Arthur Allignol <arthur.allignol@uni-ulm.de>
2015-02-12 17:52:06 +0100

    speedy but weird segfaults

--- src/gen_msm.cpp
+++ src/gen_msm.cpp
@@ -24,33 +24,48 @@ RcppExport SEXP gen_msm(SEXP _times,
     const int lt = times.size();
     const int n = entry.size();
     const int nstate = Rcpp::as<int>(_nstate);
+
     //const int cova = Rcpp::as<int>(_covariance);
 
     // define the matrices we need
-    cube nrisk(nstate, nstate, lt); nrisk.zeros();
+    mat nrisk(nstate, lt); nrisk.zeros();
     cube nev(nstate, nstate, lt); nev.zeros();
     cube dna(nstate, nstate, lt); dna.zeros();
 //    ProfilerStart("/tmp/gen_msm.prof");
+
+    for (int i=0; i < n; ++i) {
+	for (int t=0; t < lt; ++t) {
+
+	    if (entry[i] < times[t] && exit[i] >= times[t]) {
+		nrisk.at(from[i] - 1, t) += 1;
+	    }
+	    if (exit[i] == times[t] && to[i] != 0) {
+		nev.at(from[i] - 1, to[1] - 1, lt) += 1;
+		break;
+	    }
+	}
+    }
     
     for (int t=0; t < lt; ++t) {
-	
-	mat tmp(dna.slice(t).begin(), nstate, nstate, false);
-	mat dn(nev.slice(t).begin(), nstate, nstate, false);
-	mat y(nrisk.slice(t).begin(), nstate, nstate, false);
-	
-	for (int i=0; i < n; ++i) {
-	    if (entry[i] < times[t] && exit[i] >= times[t]) {
-	    	y.row(from[i] - 1) += 1;
+	for (int i=0; i < nstate; ++i) {
+	    for (int j=0; j<nstate; ++j) {
+		if (nrisk.at(i, t) != 0) {
+		    dna.at(i, j, t) = nev.at(i, j, t) / nrisk.at(i, t);
+		}
 	    }
-	    if (exit[i] == times[t] && to[i] != 0)
-	    	dn.at(from[i] - 1, to[i] - 1) += 1;
 	}
-	
-	tmp = dn / y;
-	tmp.elem(find_nonfinite(tmp)).zeros();
-	vec d = sum(tmp, 1);
-	tmp.diag() = -d;
     }
+			
+     	// mat tmp(dna.slice(t).begin(), nstate, nstate, false);
+	// mat dn(nev.slice(t).begin(), nstate, nstate, false);
+	// mat y(nrisk.slice(t).begin(), nstate, nstate, false);
+
+    	// tmp = dn / y;
+	    
+    	// tmp.elem(find_nonfinite(tmp)).zeros();
+    	// vec d = sum(tmp, 1);
+    	// tmp.diag() = -d;
+    
 
     cube est = prodint(dna, nstate, lt);	
 //    ProfilerStop();
