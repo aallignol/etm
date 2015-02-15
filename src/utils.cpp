d35428656c429c560494ad00b9f3433192753e3d
Arthur Allignol <arthur.allignol@uni-ulm.de>
2015-02-15 18:04:21 +0100

    nothing new

--- src/utils.cpp
+++ src/utils.cpp
@@ -16,6 +16,30 @@ cube prodint(const cube & dna, int nstate, int ltimes) {
     return aj;
 }
 
+// cube prodint(const cube & dna, int nstate, int ltimes) {
+
+//     cube aj(dna.begin(), nstate, nstate, ltimes);
+//     mat I = eye<mat>(nstate, nstate);
+
+//     aj.slice(0) = aj.slice(0) + I;
+
+//     cube::slice_iterator c = aj.begin_slice(1);
+//     cube::slice_iterator d = aj.end();
+//     cube::iterator a = aj.begin();
+//     cube::slice_iterator b = aj.begin_slice(ltimes - 1);
+
+//     cube::iterator i; cube::iterator j;
+//     for (i=c, j=a; i!=d; i += nstate * nstate, j += nstate * nstate) {
+	
+// 	mat bouh(i, nstate, nstate, false);
+// 	mat yah(j, nstate, nstate, false);
+
+// 	bouh = yah * (I + bouh);
+//     }
+
+//     return aj;
+// }
+
 cube deltaNA(const cube & nev, const mat & nrisk, int nstate, int ltimes) {
 
     cube dna(nstate, nstate, ltimes);
