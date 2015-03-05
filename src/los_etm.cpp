787b4361fa1811fc0b8b0ba4b2ca93fce6792e32
Arthur Allignol <arthur.allignol@uni-ulm.de>
2015-03-05 17:47:29 +0100

    compilable implementation of los

--- src/los_etm.cpp
+++ src/los_etm.cpp
@@ -16,7 +16,7 @@ RcppExport SEXP los_nocp(SEXP __times,
     Rcpp::IntegerVector Dims = _tr_mat.attr("dim");
     const int aw = Rcpp::as<int>(__aw);
     const int ratio = Rcpp::as<int>(__ratio);
-    const double = Rcpp::as<double>(__tau);
+    const double tau = Rcpp::as<double>(__tau);
     const int lt = Dims[2];
     const int nstate = Dims[0];
     cube tr_mat(_tr_mat.begin(), nstate, nstate, lt, false);
@@ -34,21 +34,29 @@ RcppExport SEXP los_nocp(SEXP __times,
 	aj.slice(i).eye();
     }
     
-    vec los0(lt), los1(lt);
+    vec los0(lt), los1(lt), a00(lt), a11(lt), a01(lt);;
+    
     los0.fill(tau);
     los1.fill(tau);
 
     for (int t = (times.size() - 2); t >= 0; --t) {
 
-	rowvec diff = times(span(t+2, lt)) - times(span(t+1, lt));
+	rowvec diff = times(span(t+2, lt)) - times(span(t+1, lt-1));
 
 	for (int j = t; j < lt; ++j) {
-	    aj.slice(j) = tr_mat_slice(t+1) * aj.slice(j);
+	    aj.slice(j) = tr_mat.slice(t+1) * aj.slice(j);
+	    a00[j] = aj.at(0, 0, j);
+	    a01[j] = aj.at(0, 1, j);
+	    a11[j] = aj.at(1, 1, j);
 	}
-
 	
+	los0[t] = times[t+1] + as_scalar(diff * (a00(span(t, lt)) + a01(span(t, lt))));
+	los1[t] = times[t+1] + as_scalar(diff * a11(span(t, lt)));
     }
 
-    
+    return Rcpp::List::create(Rcpp::Named("los0") = los0,
+			      Rcpp::Named("los0") = los1);
+
+}
     
     
