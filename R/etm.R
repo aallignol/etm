7fe314358e29e3f7fde071e225890764ff9d50da
Arthur Allignol <arthur.allignol@uni-ulm.de>
2015-06-11 17:19:37 +0200

    some more data.table modif in etm.data.frame

--- R/etm.R
+++ R/etm.R
@@ -2,6 +2,134 @@ etm <- function(x, ...) {
     UseMethod("etm")
 }
 
+
+etm.data.frame <- function(x, state.names, tra, cens.name, s, t="last",
+                           covariance=TRUE, delta.na = TRUE, modif = FALSE,
+                           alpha = 1/4, c = 1, strat_variable) {
+
+    if (missing(x))
+        stop("Argument 'x' (the data) is missing with no default")
+    if (missing(tra))
+        stop("Argument 'tra' is missing with no default")
+    if (missing(state.names))
+        stop("Argument 'state.names' is missing with no default")
+    if (missing(cens.name))
+        stop("Argument 'cens.name' is missing with no default")
+    if (missing(s))
+        stop("Argument 's' is missing with no default")
+    if (!is.data.frame(x))
+        stop("Argument 'x' must be a data.frame")
+    if (!(xor(sum(c("id", "from", "to", "time") %in% names(x)) != 4,
+              sum(c("id", "from", "to", "entry", "exit") %in% names(x)) != 5)))
+        stop("'data' must contain the right variables")
+    if (nrow(tra) != ncol(tra))
+        stop("Argument 'tra' must be a quadratic  matrix.")
+    if (sum(diag(tra)) > 0)
+        stop("transitions into the same state are not allowed")
+    if (nrow(tra) != length(state.names)) {
+        stop("The row number of 'tra' must be equal to the number of states.")
+    }
+    if (!is.logical(tra)) {
+        stop("'tra' must be a matrix of logical values, which describes the possible transitions.")
+    }
+    if (length(state.names) != length(unique(state.names))) {
+        stop("The state names must be unique.")
+    }
+    if (!(is.null(cens.name))) {
+        if (cens.name %in% state.names) {
+            stop("The name of the censoring variable just is a name of the model states.")
+        }
+    }
+    if (missing(strat_variable)) {
+        strat_var = ""
+    } else {
+        if (!all(strat_variable %in% names(x)))
+            stop("Stratification variables not in data")
+        strat_var <- strat_variable
+    }
+
+    ## if modif TRUE, check that the model is competing risks. else
+    ## set to false and issue a warning
+    if (modif == TRUE && covariance == TRUE) {
+        ## check for competing risks
+        tr.cp <- tra_comp(length(state.names) - 1)
+        if (any(dim(tra) != dim(tr.cp)) | (all(dim(tra) == dim(tr.cp)) && !all(tra == tr.cp))) {
+            covariance <- FALSE
+            warning("The variance of the estimator with the Lay and Ying transformation is only computed for competing risks data")
+        }
+    }
+
+    ## Transform x to data.table and keep only the variables we need
+    x <- data.table(x)
+    
+    reg <- names(dt)
+    names_msm <- intersect(c("id", "entry", "exit", "time", "from", "to", strat_var), reg)
+    x <- x[, names_msm, with = FALSE]
+
+    ### transitions
+    colnames(tra) <- rownames(tra) <- state.names
+    t.from <- lapply(1:dim(tra)[2], function(i) {
+        rep(rownames(tra)[i], sum(tra[i, ]))
+    })
+    t.from <- unlist(t.from)
+    t.to <- lapply(1:dim(tra)[2], function(i) {
+        colnames(tra)[tra[i, ]==TRUE]
+    })
+    t.to <- unlist(t.to)
+    trans <- data.frame(from=t.from, to=t.to)
+    namen <- paste(trans[, 1], trans[, 2])
+    
+    ## test on transitions
+    test <- x[, unique(paste(from, to))]
+    if (!(is.null(cens.name))) {
+        ref <- c(paste(trans$from, trans$to), paste(unique(trans$from), cens.name))
+    } else {
+        ref <- paste(trans$from, trans$to)
+    }
+    ref.wo.cens <- paste(trans$from, trans$to)
+    if (!(all(test %in% ref)==TRUE))
+        stop("There is undefined transitions in the data set")
+    if (x[, sum(as.character(from) == as.character(to))] > 0)
+        stop("Transitions into the same state are not allowed")
+    if (!(all(ref.wo.cens %in% test) == TRUE))
+        warning("You may have specified more possible transitions than actually present in the data")
+
+    n <- length(unique(x$id))
+    
+### data.table transformation
+    x[, id := if (is.character(id)) as.factor(id) else id]
+    if (!(is.null(cens.name))) {
+        x[, from := factor(from, levels = c(cens.name, state.names), ordered = TRUE)]
+        levels(x$from) <- 0:length(state.names)
+        x[, to := factor(to, levels = c(cens.name, state.names), ordered = TRUE)]
+        levels(x$to) <- 0:length(state.names)
+    } else{
+        x[, from := factor(from, levels = state.names, ordered = TRUE)]
+        levels(x$from) <- 1:length(state.names)
+        x[, to := factor(x$to, levels = state.names, ordered = TRUE)]
+        levels(x$to) <- 1:length(state.names)
+    }
+
+### if not, put like counting process data
+    if ("time" %in% names(x)) {
+        setorder(x, id, time)
+        x[, idd := as.integer(id)]
+        x[, masque := rbind(1, apply(as.matrix(idd), 2, diff))]
+        x[, entree := c(0, time[1:(length(time) - 1)]) * (masque == 0)]
+        x[, ':='(entry = entree,
+                 exit = time,
+                 entree = NULL,
+                 time = NULL,
+                 masque = NULL)]
+        if (sum(x$entry < x$exit) != nrow(x))
+            stop("Exit time from a state must be > entry time")
+    } else {
+        if (sum(x$entry < x$exit) != nrow(x))
+            stop("Exit time from a state must be > entry time")
+    }
+
+    
+
 etm.data.frame <- function(x, state.names, tra, cens.name, s, t="last",
                            covariance=TRUE, delta.na = TRUE, modif = FALSE,
                            alpha = 1/4, c = 1) {
